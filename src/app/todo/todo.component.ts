73984448e826d09c205fd2c1da39ea13688fd931
unknown <momin.naeemakhtar@gmail.com>
2019-10-19 18:00:55 +0530

    removed flaws

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -16,15 +16,11 @@ export class TodoComponent implements OnInit {
 
   addTask () {
     if (this.updateid !== undefined) {
-      for (let i = 0; i < this.taskList.length; i++) {
-        if (this.taskList[i].id == this.updateid) this.taskList.splice(this.updateid, 0, this.task);
-      }
-      console.log(this.updateid, this.task);
-      this.taskList.splice(this.updateid, 0, this.task);
+      for (let i = 0; i < this.taskList.length; i++)
+        if (this.taskList[i].id == this.updateid) this.taskList[i].task = this.task;
     }
-    else {
+    else
       this.taskList.push({"id": this.taskList.length+1, "task": this.task});
-    }
     this.task = "";
   }
 
@@ -32,12 +28,10 @@ export class TodoComponent implements OnInit {
     if (this.taskList.length === id-1 ) {
       this.task = this.taskList[id];
       this.updateid = this.taskList[id];
-      this.taskList.pop();
     }
     else {
       this.task = this.taskList[id-1].task;
       this.updateid = this.taskList[id-1].id;
-      this.taskList.splice(id-1, 1);
     }
   }
 
